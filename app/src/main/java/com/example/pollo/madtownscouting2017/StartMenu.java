bb89ef84ed2af362b3c03920a2faf3a3d810bbe1
DanielGomez1323 <daniel_mgomez@yahoo.com>
2017-01-10 16:13:22 -0800

    Initial Commit
    Had some trouble but we got it down.

---
+++ app/src/main/java/com/example/pollo/madtownscouting2017/StartMenu.java
@@ -0,0 +1,13 @@
+package com.example.pollo.madtownscouting2017;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_menu);
+    }
+}
