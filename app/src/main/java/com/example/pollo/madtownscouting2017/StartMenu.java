59de0ffc976662648a91005203bd26a2506afe41
Juli1323 <ah503917@gmail.com>
2017-01-10 17:24:16 -0800

    Added Buttons
    I added buttons to the code.

--- app/src/main/java/com/example/pollo/madtownscouting2017/StartMenu.java
+++ app/src/main/java/com/example/pollo/madtownscouting2017/StartMenu.java
@@ -2,12 +2,31 @@ package com.example.pollo.madtownscouting2017;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class StartMenu extends AppCompatActivity {
+
+    EditText searchMenuSearchBox;
+    Button seacrhButton;
+    Button scoutButton;
+    Button uploadData;
+    Button teamsView;
+    Button addPhoto;
+    Button viewPhoto;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_menu);
+        EditText searchMenuSearchBox = (EditText) findViewById(R.id.searchMenuSearchBox);
+        Button searchButton = (Button) findViewById(R.id.searchButton);
+        Button scoutButton = (Button) findViewById(R.id.scoutButton);
+        Button uploadData = (Button) findViewById(R.id.uploadData);
+        Button teamsView = (Button) findViewById(R.id.teamsView);
+        Button addPhoto = (Button) findViewById(R.id.addPhoto);
+        Button viewPhoto = (Button) findViewById(R.id.viewPhoto);
     }
 }
