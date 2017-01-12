330e4d144dd219df9c89089bb14583cc0ce896be
IsaacNewton321 <19IsaacD740@maderak12.org>
2017-01-11 17:50:41 -0800

    Added button functionality

---
+++ app/src/main/java/com/example/pollo/madtownscouting2017/teamsView.java
@@ -0,0 +1,13 @@
+package com.example.pollo.madtownscouting2017;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class teamsView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teams_view);
+    }
+}
