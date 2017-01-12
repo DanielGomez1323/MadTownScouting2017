0f072ea890bc04739478bbf16ab7d92a62d5dbce
IsaacNewton321 <19IsaacD740@maderak12.org>
2017-01-11 19:13:54 -0800

    Layout for auto

--- app/src/main/java/com/example/pollo/madtownscouting2017/AutoPeriod.java
+++ app/src/main/java/com/example/pollo/madtownscouting2017/AutoPeriod.java
@@ -1,7 +1,13 @@
 package com.example.pollo.madtownscouting2017;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class AutoPeriod extends AppCompatActivity {
 
@@ -9,5 +15,38 @@ public class AutoPeriod extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auto_period);
+
+        CheckBox baselineCheckBox = (CheckBox) findViewById(R.id.baselineCheckBox);
+        CheckBox gearAttemptCheckBox = (CheckBox) findViewById(R.id.gearAttemptCheckBox);
+        CheckBox gearSuccessCheckBox = (CheckBox) findViewById(R.id.gearSuccessCheckBox);
+        CheckBox highCheckBox = (CheckBox) findViewById(R.id.highCheckBox);
+        CheckBox lowCheckBox = (CheckBox) findViewById(R.id.lowCheckBox);
+        SeekBar autoBallsSeekBar = (SeekBar) findViewById(R.id.autoBallsSeekBar);
+        final TextView ballCountTextView = (TextView) findViewById(R.id.ballCountTextView);
+        Button continueButton = (Button) findViewById(R.id.continueButton);
+
+        autoBallsSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                ballCountTextView.setText("Balls made: " + progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        continueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent teleIntent = new Intent(getApplicationContext(), TeleopPeriod.class);
+                startActivity(teleIntent);
+            }
+        });
     }
 }
