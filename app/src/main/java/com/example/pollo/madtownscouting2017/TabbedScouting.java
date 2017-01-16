1b83ca691311d8eae1affa6fa8f528762813dfea
IsaacNewton321 <19IsaacD740@maderak12.org>
2017-01-16 13:05:12 -0800

    trying immersive mode

--- app/src/main/java/com/example/pollo/madtownscouting2017/TabbedScouting.java
+++ app/src/main/java/com/example/pollo/madtownscouting2017/TabbedScouting.java
@@ -62,6 +62,14 @@ public class TabbedScouting extends AppCompatActivity {
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
 
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
