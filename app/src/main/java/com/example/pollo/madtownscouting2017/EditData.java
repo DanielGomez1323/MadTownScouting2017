66b52b131ea883c775f4f2c0203fc06b21911b3c
DanielGomez1323 <daniel_mgomez@yahoo.com>
2017-01-28 17:37:04 -0800

    Finished the code aspect

--- app/src/main/java/com/example/pollo/madtownscouting2017/EditData.java
+++ app/src/main/java/com/example/pollo/madtownscouting2017/EditData.java
@@ -1,7 +1,13 @@
 package com.example.pollo.madtownscouting2017;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -9,97 +15,135 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 public class EditData extends AppCompatActivity {
-    TextView teamNumberTV;
     EditText teamNumberEnterText;
-    TextView matchNumberTV;
     EditText matchNumberEnterText;
-    TextView teamColorTV;
     EditText teamColorEnterText;
-    TextView crossedBaselineTV;
     EditText baselineCrossedEnterText;
-    TextView autoGearAttemptTV;
     EditText autoGearAttemptEnterText;
-    TextView autoGearSuccessTV;
     EditText autoGearSuccessEnterText;
-    TextView autoHighScoredTV;
     EditText autoHighScoredEnterText;
-    TextView autoHighMissedTV;
     EditText autoHighMissedEnterText;
-    TextView autoLowScoredTV;
     EditText autoLowScoredEnterText;
-    TextView autoLowMissedTV;
     EditText autoLowMissedEnterText;
-    TextView gearsPickedUpTV;
     EditText gearsPickedUpEnterText;
-    TextView gearsHungTV;
     EditText gearsHungEnterText;
-    TextView gearsDroppedTV;
     EditText gearsDroppedEnterText;
-    TextView highShootSpeedTV;
     EditText highShootSpeedEnterText;
-    TextView highShotsMissedTV;
     EditText highShotsMissedEnterText;
-    TextView lowShootSpeedTV;
     EditText lowShootSpeedEnterText;
-    TextView lowShotsMissedTV;
     EditText lowShotsMissedEnterText;
-    TextView hopperIntakeTV;
     EditText hopperIntakeEnterText;
-    TextView climbTimeTV;
     EditText climbTimeEnterText;
-    TextView climbSuccessTV;
     EditText climbSuccessEnterText;
-    TextView tbhTV;
     EditText tbhEnterText;
+
     Button confirmEditsButton;
+
+    String tNumber;
+    String mNumber;
+
+    SQLiteDatabase myDB = null;
+    Cursor c;
+    int id;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_data);
-        teamNumberTV = (TextView)findViewById(R.id.teamNumberTV);
         teamNumberEnterText = (EditText)findViewById(R.id.teamNumberEnterText);
-        matchNumberTV = (TextView)findViewById(R.id.matchNumberTV);
         matchNumberEnterText = (EditText)findViewById(R.id.matchNumberEnterText);
-        teamColorTV = (TextView)findViewById(R.id.teamColorTV);
         teamColorEnterText = (EditText)findViewById(R.id.teamColorEnterText);
-        crossedBaselineTV = (TextView)findViewById(R.id.crossedBaselineTV);
         baselineCrossedEnterText = (EditText)findViewById(R.id.baselineCrossedEnterText);
-        autoGearAttemptTV = (TextView)findViewById(R.id.autoGearAttemptTV);
         autoGearAttemptEnterText = (EditText)findViewById(R.id.autoGearAttemptEnterText);
-        autoGearSuccessTV = (TextView)findViewById(R.id.autoGearSuccessTV);
         autoGearSuccessEnterText = (EditText)findViewById(R.id.autoGearSuccessEnterText);
-        autoHighScoredTV = (TextView)findViewById(R.id.autoHighScoredTV);
         autoHighScoredEnterText = (EditText)findViewById(R.id.autoHighScoredEnterText);
-        autoHighMissedTV = (TextView)findViewById(R.id.autoHighMissedTV);
         autoHighMissedEnterText = (EditText)findViewById(R.id.autoHighMissedEnterText);
-        autoLowScoredTV = (TextView)findViewById(R.id.autoLowScoredTV);
         autoLowScoredEnterText = (EditText)findViewById(R.id.autoLowScoredEnterText);
-        autoLowMissedTV = (TextView)findViewById(R.id.autoLowMissedTV);
         autoLowMissedEnterText = (EditText)findViewById(R.id.autoLowMissedEnterText);
-        gearsPickedUpTV = (TextView)findViewById(R.id.gearsPickedUpTV);
         gearsPickedUpEnterText = (EditText)findViewById(R.id.gearsPickedUpEnterText);
-        gearsHungTV = (TextView)findViewById(R.id.gearsHungTV);
         gearsHungEnterText = (EditText)findViewById(R.id.gearsHungEnterText);
-        gearsDroppedTV = (TextView)findViewById(R.id.gearsDroppedTV);
         gearsDroppedEnterText = (EditText)findViewById(R.id.gearsDroppedEnterText);
-        highShootSpeedTV = (TextView)findViewById(R.id.highShootSpeedTV);
         highShootSpeedEnterText = (EditText)findViewById(R.id.highShootSpeedEnterText);
-        highShotsMissedTV = (TextView)findViewById(R.id.highShotsMissedTV);
         highShotsMissedEnterText = (EditText)findViewById(R.id.highShotsMissedEnterText);
-        lowShootSpeedTV = (TextView)findViewById(R.id.lowShootSpeedTV);
         lowShootSpeedEnterText = (EditText)findViewById(R.id.lowShootSpeedEnterText);
-        lowShotsMissedTV = (TextView)findViewById(R.id.lowShotsMissedTV);
         lowShotsMissedEnterText = (EditText)findViewById(R.id.lowShotsMissedEnterText);
-        hopperIntakeTV = (TextView)findViewById(R.id.hopperIntakeTV);
         hopperIntakeEnterText = (EditText)findViewById(R.id.hopperIntakeEnterText);
-        climbTimeTV = (TextView)findViewById(R.id.climbTimeTV);
         climbTimeEnterText = (EditText) findViewById(R.id.climbTimeEnterText);
-        climbSuccessTV = (TextView)findViewById(R.id.climbSuccessTV);
         climbSuccessEnterText = (EditText)findViewById(R.id.climbSuccessEnterText);
-        tbhTV = (TextView)findViewById(R.id.tbhTV);
         tbhEnterText = (EditText)findViewById(R.id.tbhEnterText);
         confirmEditsButton = (Button)findViewById(R.id.confirmEditsButton);
+
+        Intent intent = getIntent();
+        id = intent.getIntExtra("ID", 0);
+        myDB = openOrCreateDatabase("FRC", MODE_PRIVATE, null);
+        c = myDB.rawQuery("SELECT * FROM SteamWorks WHERE _id =" + id, null);
+        c.moveToFirst();
+        int i = c.getCount();
+        if(i > 0){
+            teamNumberEnterText.setText(c.getString(c.getColumnIndex("teamNumber")), TextView.BufferType.EDITABLE);
+            matchNumberEnterText.setText(c.getString(c.getColumnIndex("matchNumber")), TextView.BufferType.EDITABLE);
+            teamColorEnterText.setText(c.getString(c.getColumnIndex("teamColor")), TextView.BufferType.EDITABLE);
+            baselineCrossedEnterText.setText(c.getString(c.getColumnIndex("baseLine")), TextView.BufferType.EDITABLE);
+            autoGearAttemptEnterText.setText(c.getString(c.getColumnIndex("autoGearAttempt")), TextView.BufferType.EDITABLE);
+            autoGearSuccessEnterText.setText(c.getString(c.getColumnIndex("autoGearSuccess")), TextView.BufferType.EDITABLE);
+            autoHighScoredEnterText.setText(c.getString(c.getColumnIndex("autoHighScored")), TextView.BufferType.EDITABLE);
+            autoHighMissedEnterText.setText(c.getString(c.getColumnIndex("autoHighMissed")), TextView.BufferType.EDITABLE);
+            autoLowScoredEnterText.setText(c.getString(c.getColumnIndex("autoLowScored")), TextView.BufferType.EDITABLE);
+            autoLowMissedEnterText.setText(c.getString(c.getColumnIndex("autoLowMissed")), TextView.BufferType.EDITABLE);
+            gearsPickedUpEnterText.setText(c.getString(c.getColumnIndex("gearsPickedUp")), TextView.BufferType.EDITABLE);
+            gearsHungEnterText.setText(c.getString(c.getColumnIndex("gearsHung")), TextView.BufferType.EDITABLE);
+            gearsDroppedEnterText.setText(c.getString(c.getColumnIndex("gearsDropped")), TextView.BufferType.EDITABLE);
+            highShootSpeedEnterText.setText(c.getString(c.getColumnIndex("highShootSpeed")), TextView.BufferType.EDITABLE);
+            highShotsMissedEnterText.setText(c.getString(c.getColumnIndex("highShotsMissed")), TextView.BufferType.EDITABLE);
+            lowShootSpeedEnterText.setText(c.getString(c.getColumnIndex("lowShootSpeed")), TextView.BufferType.EDITABLE);
+            lowShotsMissedEnterText.setText(c.getString(c.getColumnIndex("lowShotsMissed")), TextView.BufferType.EDITABLE);
+            hopperIntakeEnterText.setText(c.getString(c.getColumnIndex("hopperIntake")), TextView.BufferType.EDITABLE);
+            climbTimeEnterText.setText(c.getString(c.getColumnIndex("climbTime")), TextView.BufferType.EDITABLE);
+            climbSuccessEnterText.setText(c.getString(c.getColumnIndex("climbSuccess")), TextView.BufferType.EDITABLE);
+            tbhEnterText.setText(c.getString(c.getColumnIndex("tbh")), TextView.BufferType.EDITABLE);
+        }
+        c.close();
+        myDB.close();
+        confirmEditsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeData();
+            }
+        });
+    }
+    public void changeData(){
+        try{
+            myDB = openOrCreateDatabase("FRC", MODE_PRIVATE, null);
+            String tbh = tbhEnterText.getText().toString();
+            tbh = tbh.replace("'","*");
+            myDB.execSQL("UPDATE SteamWorks SET teamNumber = " + teamNumberEnterText.getText().toString() +
+            ", matchNumber = " + matchNumberEnterText.getText().toString() + ", teamColor = " +
+            teamColorEnterText.getText().toString() + ", baseLine = " + baselineCrossedEnterText.getText().toString() +
+            ", autoGearAttempt = " + autoGearAttemptEnterText.getText().toString() + ", autoGearSuccess = " +
+            autoGearSuccessEnterText.getText().toString() + ", autoHighScored = " + autoHighScoredEnterText.getText().toString() +
+            ", autoHighMissed = " + autoHighMissedEnterText.getText().toString() + ", autoLowScored = " +
+            autoLowScoredEnterText.getText().toString() + ", autoLowMissed = " + autoLowMissedEnterText.getText().toString() +
+            ", gearsPickedUp = " + gearsPickedUpEnterText.getText().toString() + ", gearsHung = " +
+            gearsHungEnterText.getText().toString() + ", gearsDropped = " + gearsDroppedEnterText.getText().toString() +
+            ", highShootSpeed = " + highShootSpeedEnterText.getText().toString() + ", highShotsMissed = " +
+            highShotsMissedEnterText.getText().toString() + ", lowShootSpeed = " + lowShootSpeedEnterText.getText().toString() +
+            ", lowShotsMissed = " + lowShotsMissedEnterText.getText().toString() + ", hopperIntake = " +
+            hopperIntakeEnterText.getText().toString() + ", climbTime = " + climbTimeEnterText.getText().toString() +
+            ", climbSuccess = " + climbSuccessEnterText.getText().toString() + ", tbh = " + tbh + "' WHERE _id = " + id);
+        }catch (SQLException e){
+            System.out.print(e);
+        }
+        Cursor cur = myDB.rawQuery("SELECT * FROM SteamWorks Where _id =" + id, null);
+        cur.moveToFirst();
+        tNumber = cur.getString(cur.getColumnIndex("teamNumber"));
+        mNumber = cur.getString(cur.getColumnIndex("teamColor"));
+        cur.close();
+        myDB.close();;
+        Intent returnIntent = new Intent();
+        returnIntent.putExtra("TEAM_NUMBER", tNumber);
+        returnIntent.putExtra("TEAM_COLOR",  mNumber);
+        setResult(Activity.RESULT_OK,returnIntent);
+        finish();
     }
 }
