ef1adb8c7fff418edb21e6488464b7068ade67a5
DanielGomez1323 <daniel_mgomez@yahoo.com>
2017-03-28 20:37:10 -0700

    Fixed the lost teamView Screeen

---
+++ app/src/main/java/com/example/pollo/madtownscouting2017/teamsView.java
@@ -0,0 +1,17 @@
+package com.example.pollo.madtownscouting2017;
+
+/**
+ * Created by Pollo on 3/28/2017.
+ */
+
+import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+
+public class teamsView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teams_view);
+    }
+}
