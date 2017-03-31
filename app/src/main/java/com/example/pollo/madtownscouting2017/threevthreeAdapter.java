454e41d410b5e40b19286786dca540ea2fbf0141
DanielGomez1323 <daniel_mgomez@yahoo.com>
2017-03-30 20:54:47 -0700

    Messed with the adapter
    This is a bit confusing

--- app/src/main/java/com/example/pollo/madtownscouting2017/threevthreeAdapter.java
+++ app/src/main/java/com/example/pollo/madtownscouting2017/threevthreeAdapter.java
@@ -3,110 +3,59 @@ package com.example.pollo.madtownscouting2017;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.util.List;
-
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONException;
+import org.apache.http.params.BasicHttpParams;
 import org.json.JSONObject;
 
-import android.util.Log;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 
 public class threevthreeAdapter extends AppCompatActivity {
 
-    // Response from the HTTP Request
-    static InputStream httpResponseStream = null;
-    // JSON Response String to create JSON Object
-    static String jsonString = "";
+    DefaultHttpClient   httpclient = new DefaultHttpClient(new BasicHttpParams());
+    HttpPost httppost = new HttpPost(www.gorohi.com/1323/2017/silicon/teamdata.php);
+// Depends on your web service
+            httppost.setHeader("Content-type", "application/json");
 
-    // Method to issue HTTP request, parse JSON result and return JSON Object
-    public JSONObject makeHttpRequest(String url, String method,
-                                      List<NameValuePair> params) {
+    InputStream inputStream = null;
+    String result = null;
+    try {
+        HttpResponse response = httpclient.execute(httppost);
+        HttpEntity entity = response.getEntity();
 
-        try {
-            // get a Http client
-            DefaultHttpClient httpClient = new DefaultHttpClient();
+        inputStream = entity.getContent();
+        // json is UTF-8 by default
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"), 8);
+        StringBuilder sb = new StringBuilder();
 
-            // If required HTTP method is POST
-            if (method == "POST") {
-                // Create a Http POST object
-                HttpPost httpPost = new HttpPost(url);
-                // Encode the passed parameters into the Http request
-                httpPost.setEntity(new UrlEncodedFormEntity(params));
-                // Execute the request and fetch Http response
-                HttpResponse httpResponse = httpClient.execute(httpPost);
-                // Extract the result from the response
-                HttpEntity httpEntity = httpResponse.getEntity();
-                // Open the result as an input stream for parsing
-                httpResponseStream = httpEntity.getContent();
-            }
-            // Else if it is GET
-            else if (method == "GET") {
-                // Format the parameters correctly for HTTP transmission
-                String paramString = URLEncodedUtils.format(params, "utf-8");
-                // Add parameters to url in GET format
-                url += "?" + paramString;
-                // Execute the request
-                HttpGet httpGet = new HttpGet(url);
-                // Execute the request and fetch Http response
-                HttpResponse httpResponse = httpClient.execute(httpGet);
-                // Extract the result from the response
-                HttpEntity httpEntity = httpResponse.getEntity();
-                // Open the result as an input stream for parsing
-                httpResponseStream = httpEntity.getContent();
-            }
-            // Catch Possible Exceptions
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (ClientProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        String line = null;
+        while ((line = reader.readLine()) != null)
+        {
+            sb.append(line + "\n");
         }
-
-        try {
-            // Create buffered reader for the httpResponceStream
-            BufferedReader httpResponseReader = new BufferedReader(
-                    new InputStreamReader(httpResponseStream, "iso-8859-1"), 8);
-            // String to hold current line from httpResponseReader
-            String line = null;
-            // Clear jsonString
-            jsonString = "";
-            // While there is still more response to read
-            while ((line = httpResponseReader.readLine()) != null) {
-                // Add line to jsonString
-                jsonString += (line + "\n");
-            }
-            // Close Response Stream
-            httpResponseStream.close();
-        } catch (Exception e) {
-            Log.e("Buffer Error", "Error converting result " + e.toString());
-        }
-
-        try {
-            // Create jsonObject from the jsonString and return it
-            return new JSONObject(jsonString);
-        } catch (JSONException e) {
-            Log.e("JSON Parser", "Error parsing data " + e.toString());
-            // Return null if in error
-            return null;
-        }
+        result = sb.toString();
+    } catch (Exception e) {
+        // Oops
     }
+    finally {
+        try{if(inputStream != null)inputStream.close();}catch(Exception squish){}
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_threevthree_adapter);
+
+
+        for (int i=0; i < jArray.length(); i++)
+        {
+            try {
+                JSONObject oneObject = jArray.getJSONObject(i);
+                
     }
 }
